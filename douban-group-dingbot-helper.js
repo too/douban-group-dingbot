4b88889a8d03aaaad6adf3c27191e81bde2c3654
Xiao Hanyu <xiaohanyu1988@gmail.com>
2016-09-21 13:55:06 +0800

    Add a helper js to drive the robot.
    Thus you can just view "https://www.douban.com/dingbot" to start the
    robot.

---
+++ douban-group-dingbot-helper.js
@@ -0,0 +1,38 @@
+// ==UserScript==
+// @name         Douban Group Dingbot Helper
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Douban Group topics auto comments.
+// @author       You
+// @match        https://www.douban.com/dingbot
+// @grant        none
+// ==/UserScript==
+
+(function() {
+  'use strict';
+
+  function random_int (min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
+  }
+
+  var urls = ["https://www.douban.com/group/topic/90845242/",
+              "https://www.douban.com/group/topic/90845094/",
+              "https://www.douban.com/group/topic/90845036/",
+              "https://www.douban.com/group/topic/90844973/",
+              "https://www.douban.com/group/topic/90844873/",
+              "https://www.douban.com/group/topic/90844341/",
+              "https://www.douban.com/group/topic/90844012/",
+              "https://www.douban.com/group/topic/90841110/",
+              "https://www.douban.com/group/topic/90840593/",
+              "https://www.douban.com/group/topic/90840457/",
+              "https://www.douban.com/group/topic/90840401/",
+              "https://www.douban.com/group/topic/90840340/",
+              "https://www.douban.com/group/topic/90840228/",
+              "https://www.douban.com/group/topic/90840096/",
+              "https://www.douban.com/group/topic/90839736/",
+              "https://www.douban.com/group/topic/90839654/"];
+
+  window.location.href = urls[random_int(0, urls.length)];
+})();
