2a020ccb02d95443826798bc322c7f46867f6f58
Xiao Hanyu <xiaohanyu1988@gmail.com>
2016-10-31 19:27:57 +0800

    New post list, also comment based on priority.

--- douban-group-dingbot.rb
+++ douban-group-dingbot.rb
@@ -1,35 +1,103 @@
 #!/usr/bin/env ruby
 
 def main
-  urls = ["https://www.douban.com/group/topic/90845242/",
-          "https://www.douban.com/group/topic/90845094/",
-          "https://www.douban.com/group/topic/90845036/",
-          "https://www.douban.com/group/topic/90844973/",
-          "https://www.douban.com/group/topic/90844873/",
-          "https://www.douban.com/group/topic/90844341/",
-          "https://www.douban.com/group/topic/90844012/",
-          "https://www.douban.com/group/topic/90841110/",
-          "https://www.douban.com/group/topic/90840593/",
-          "https://www.douban.com/group/topic/90840457/",
-          "https://www.douban.com/group/topic/90840401/",
-          "https://www.douban.com/group/topic/90840340/",
-          "https://www.douban.com/group/topic/90840228/",
-          "https://www.douban.com/group/topic/90840096/",
-          "https://www.douban.com/group/topic/90839736/",
-          "https://www.douban.com/group/topic/90839654/",
-          "https://www.douban.com/group/topic/90839557/"];
+  doctor_urls = [{:url => "https://www.douban.com/group/topic/92555668/",
+                  :frequency => 20,
+                  :tag => "beijing"},
+                 {:url => "https://www.douban.com/group/topic/92556693/",
+                  :frequency => 5,
+                  :tag => "beijing"},
+                 {:url => "https://www.douban.com/group/topic/92556868/",
+                  :frequency => 1,
+                  :tag => "internet"},
+                 {:url => "https://www.douban.com/group/topic/92557047/",
+                  :frequency => 2,
+                  :tag => "campus"},
+                 {:url => "https://www.douban.com/group/topic/92557159/",
+                  :frequency => 1,
+                  :tag => "shanghai-internship"},
+                 {:url => "https://www.douban.com/group/topic/92557288/",
+                  :frequency => 15,
+                  :tag => "shenzhen"},
+                 {:url => "https://www.douban.com/group/topic/92557393/",
+                  :frequency => 1,
+                  :tag => "internet"},
+                 {:url => "https://www.douban.com/group/topic/92557523/",
+                  :frequency => 18,
+                  :tag => "shanghai"},
+                 {:url => "https://www.douban.com/group/topic/92558006/",
+                  :frequency => 2,
+                  :tag => "design"},
+                 {:url => "https://www.douban.com/group/topic/92561303/",
+                  :frequency => 2,
+                  :tag => "foreign-company"},
+                 {:url => "https://www.douban.com/group/topic/92561439/",
+                  :frequency => 1,
+                  :tag => "foreign-company2"},
+                 {:url => "https://www.douban.com/group/topic/92561628/",
+                  :frequency => 1,
+                  :tag => "english-resume"},
+                 {:url => "https://www.douban.com/group/topic/92561816/",
+                  :frequency => 4,
+                  :tag => "campus-student"},
+                 {:url => "https://www.douban.com/group/topic/92561998/",
+                  :frequency => 4,
+                  :tag => "shenzhen"},
+                 {:url => "https://www.douban.com/group/topic/92562193/",
+                  :frequency => 10,
+                  :tag => "guangzhou"},
+                 {:url => "https://www.douban.com/group/topic/92562583/",
+                  :frequency => 4,
+                  :tag => "chengdu"},
+                 {:url => "https://www.douban.com/group/topic/92562698/",
+                  :frequency => 2,
+                  :tag => "chengdu"},
+                 {:url => "https://www.douban.com/group/topic/92563085/",
+                  :frequency => 2,
+                  :tag => "abroad"},
+                 {:url => "https://www.douban.com/group/topic/92563181/",
+                  :frequency => 5,
+                  :tag => 'usa'},
+                 {:url => "https://www.douban.com/group/topic/92563447/",
+                  :frequency => 4,
+                  :tag => "british"},
+                 {:url => "https://www.douban.com/group/topic/92563604/",
+                  :frequency => 3,
+                  :tag => "austrilia"},
+                 {:url => "https://www.douban.com/group/topic/92563776/",
+                  :frequency => 2,
+                  :tag => "tofel"},
+                 {:url => "https://www.douban.com/group/topic/92563855/",
+                  :frequency => 2,
+                  :tag => "abroad"},
+                 {:url => "https://www.douban.com/group/topic/92564015/",
+                  :frequency => 1,
+                  :tag => "austrilia"},
+                 {:url => "https://www.douban.com/group/topic/92564169/",
+                  :frequency => 2,
+                  :tag => "europe"},
+                 {:url => "https://www.douban.com/group/topic/92564362/",
+                  :frequency => 2,
+                  :tag => "abroad"},
+                 {:url => "https://www.douban.com/group/topic/92564555/",
+                  :frequency => 2,
+                  :tag => "canada"}]
 
-  rand_url = urls[rand(urls.length)]
+  urls = doctor_urls.map { |url| [url[:url]] * url[:frequency] }.flatten
 
-  sleep_seconds = rand(1000)
-  puts "Sleep #{sleep_seconds} for a while."
-  sleep(sleep_seconds)
 
-  case RUBY_PLATFORM
-  when /darwin/
-    `open #{rand_url}`
-  when /linux/
-    `xdg-open #{rand_url}`
+  3.times do
+    sleep_seconds = rand(1500)
+    rand_url = urls[rand(urls.length)]
+    puts "Sleep #{sleep_seconds} for a while."
+    sleep(sleep_seconds)
+
+    case RUBY_PLATFORM
+    when /darwin/
+      `open #{rand_url}`
+    when /linux/
+      `xdg-open #{rand_url}`
+    end
   end
 end
 
